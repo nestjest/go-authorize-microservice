2f604018bfd8e138a0d54b3694c19a9f0981b838
nestjest <kockers@vk.com>
2025-10-22 15:10:07 +0300

    Update README.md to include an image and improve formatting

---
+++ sso/internal/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	TokenTTL time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC GRPCConfig `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int `yaml:"port"`
+	TimeOut time.Duration `yaml:"timeout"`
+}
